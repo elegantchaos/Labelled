7c977d980c93efc86f8b25c60b5544fe2c19395c
Sam Deane <sam@elegantchaos.com>
2021-09-07 12:02:02 +0100

    Initial implementation

--- Sources/Labelled/Labelled.swift
+++ Sources/Labelled/Labelled.swift
@@ -3,6 +3,28 @@
 //  All code (c) 2021 - present day, Elegant Chaos.
 // -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
 
-struct Labelled {
-    var text = "Hello, World!"
+import SwiftUI
+import SwiftUIExtensions
+
+public protocol Labelled: Hashable, Identifiable {
+    var icon: String { get }
+    var label: String { get }
+}
+
+public extension Labelled {
+    var icon: String { "" }
+}
+
+public extension Labelled where Self: CustomStringConvertible {
+    var label: String { description }
+}
+
+public extension Labelled where Self: RawRepresentable, Self.RawValue == String {
+    var label: String { rawValue }
+}
+
+public extension Label {
+    init(_ labelled: Labelled) {
+        self.init(labelled.label, systemImage: labelled.icon)
+    }
 }
